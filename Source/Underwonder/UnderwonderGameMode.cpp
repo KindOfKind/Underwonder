c0c103c9b2144c5064b47604c56ed700ec641833
KindOfKind <63473797+KindOfKind@users.noreply.github.com>
2022-04-19 13:51:45 +0300

    Folders structure is corrected, new level with basic light setup is created.

--- Source/Underwonder/UnderwonderGameMode.cpp
+++ Source/Underwonder/UnderwonderGameMode.cpp
@@ -7,7 +7,7 @@
 AUnderwonderGameMode::AUnderwonderGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Underwonder/Core/Player/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
